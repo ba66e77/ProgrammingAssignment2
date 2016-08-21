7db0fb722169a4d2200456d95eab6489d5793027
Barrett Smith <barrett_m_smith@oa.eop.gov>
2016-08-21 10:13:46 -0400

    Initial development of assignment functions.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(inputMatrix = matrix()) {
+    invertedMatrix <- NULL
+    setMatrix <- function(y) {
+        inputMatrix <<- y
+        invertedMatrix <<- NULL
+    }
+    getMatrix <- function() inputMatrix
+    setInvertedMatrix <- function(mean) invertedMatrix <<- mean
+    getInvertedMatrix <- function() invertedMatrix
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setInvertedMatrix = setInvertedMatrix,
+         getInvertedMatrix = getInvertedMatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(makeCacheMatrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+    invertedMatrix <- makeCacheMatrix$getInvertedMatrix()
+    if(!is.null(invertedMatrix)) {
+        message("getting cached data")
+        return(invertedMatrix)
+    }
+    data <- makeCacheMatrix$getInvertedMatrix()
+    invertedMatrix <- solve(data, ...)
+    makeCacheMatrix$setInvertedMatrix(invertedMatrix)
+    invertedMatrix
 }
